eb2dff9349aaa7517646d18f551cb13f48621ca6
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-26 19:09:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-date: applicationDate.value,
-ar input fields
-        jobTitle.value = "";
-        company.value = "";
-        applicationDate.value = "";
-        status.value = "Applied";
-    });
 
-    // Render jobs on page load
-    renderJobs();
-});
