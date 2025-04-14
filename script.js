b892b05b8072ca864cb94e56b583134174688787
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-04-14 15:00:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // DOM Elements
-    const jobContainer = document.getElementById("jobContainer");
-    const jobForm = document.getElementById("jobForm");
+const jobForm = document.getElementById("jobForm");
     const jobModal = document.getElementById("jobModal");
     const detailsModal = document.getElementById("detailsModal");
     const closeBtns = document.querySelectorAll(".close-btn");
